886245e67640126563320cad69a396198929b3c4
66993042Missy <162462701+66993042Missy@users.noreply.github.com>
2024-04-24 15:10:36 +0200

    Add files via upload

---
+++ leseli's relay.ino
@@ -0,0 +1,41 @@
+//relay
+
+//Email:info@primerobotics.in
+
+//Website:www.primerobotics.in
+
+/******************************************************/
+
+const int relayPin
+= 8; //the base of the transistor attach to
+
+/******************************************************/
+
+void setup()
+
+{
+
+  pinMode(relayPin, OUTPUT); //initialize the
+//relayPin as an output;
+
+}
+
+/******************************************************/
+
+void loop()
+
+{
+
+  digitalWrite(relayPin, HIGH); //drive relay
+//closure conduction;
+
+  delay(1000); //wait for a second
+
+  digitalWrite(relayPin, LOW); //drive the
+//relay is closed off;
+
+  delay(1000); //wait for a second
+
+}
+
+/******************************************************/
